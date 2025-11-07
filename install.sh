ce9bfeb2bbe7c66b63cdc0bc9e6541f8996f594f
Sergio Sanchez <ssancd03@estudiantes.unileon.es>
2025-11-07 12:28:46 +0100

    p3

--- install.sh
+++ install.sh
@@ -8,8 +8,28 @@ echo "###### 1 Visual Studio Code"
 echo "########################################################################"
 echo
 
-apt install -y apt-transport-https wget
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /usr/share/keyrings/packages.microsoft.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list
-apt update
-apt install -y code
+sudo apt-get install wget gpg -y
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+rm -f packages.microsoft.gpg
+
+sudo apt install -y apt-transport-https
+sudo apt update
+sudo apt install -y code 
+
+echo -e "${GREEN}========================${RESET}"
+echo -e "${GREEN}  Installing Chrome     ${RESET}"
+echo -e "${GREEN}========================${RESET}"
+wget -qO- https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor > google-chrome.gpg
+sudo install -D -o root -g root -m 644 google-chrome.gpg /etc/apt/keyrings/google-chrome.gpg
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list > /dev/null
+rm -f google-chrome.gpg
+
+sudo apt update
+sudo apt install -y google-chrome-stable
+
+echo -e "${YELLOW}================================${RESET}"
+echo -e "${YELLOW} Installing GNOME Tools and Git ${RESET}"
+echo -e "${YELLOW}================================${RESET}"
+sudo apt install -y gnome-control-center git
