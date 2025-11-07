a36b916922c11ec40caabcdfe21179ab1ff67d70
Sergio Sanchez <ssancd03@estudiantes.unileon.es>
2025-11-07 12:20:59 +0100

    p1

--- install.sh
+++ install.sh
@@ -2,24 +2,6 @@
 #!/bin/bash
 # set -e
 
-
-installed_dir=$(dirname $(readlink -f $(basename `pwd`)))
-
-##################################################################################################################################
-
-if [ "$DEBUG" = true ]; then
-    echo
-    echo "------------------------------------------------------------"
-    echo "Running $(basename $0)"
-    echo "------------------------------------------------------------"
-    echo
-    echo "Debug mode is on. Press Enter to continue..."
-    read dummy
-    echo
-fi
-
-##################################################################################################################################
-
 echo
 echo "########################################################################"
 echo "###### 1 Visual Studio Code"
@@ -31,29 +13,3 @@ wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /u
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list
 apt update
 apt install -y code
-
-echo
-echo "########################################################################"
-echo "###### 2 Google Chrome"
-echo "########################################################################"
-echo
-
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor > /usr/share/keyrings/google-chrome.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
-apt update
-apt install -y google-chrome-stable
-
-echo
-echo "########################################################################"
-echo "###### 3 Docker"
-echo "########################################################################"
-echo
-
-apt install -y ca-certificates curl
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt update
-apt install docker-ce -y
-groupmod -g 1000 docker
