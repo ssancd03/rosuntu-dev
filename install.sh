269560b7d3daa347493f5e011874add2b1a0eaae
Sergio Sanchez <ssancd03@estudiantes.unileon.es>
2025-11-10 12:48:07 +0100

    update

--- install.sh
+++ install.sh
@@ -54,12 +54,9 @@ echo -e "${BLUE}========================================${RESET}"
 echo -e "${BLUE}  Installing Docker${RESET}"
 echo -e "${BLUE}========================================${RESET}"
 echo
-apt-get update
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+rm get-docker.sh
 groupmod -g 1000 docker
 
 # echo
