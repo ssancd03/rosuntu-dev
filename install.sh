892aef744e6d01dd42c73248bb52836b80963071
Sergio Sanchez <ssancd03@estudiantes.unileon.es>
2025-11-10 12:42:38 +0100

    updated install

--- install.sh
+++ install.sh
@@ -54,13 +54,12 @@ echo -e "${BLUE}========================================${RESET}"
 echo -e "${BLUE}  Installing Docker${RESET}"
 echo -e "${BLUE}========================================${RESET}"
 echo
-apt-get install ca-certificates curl
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt update
-apt install docker-ce -y
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
 groupmod -g 1000 docker
 
 # echo
