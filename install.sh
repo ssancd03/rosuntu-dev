e6c6447bfab79dbbc5875e98e2ef1743f836e579
Sergio Sanchez <ssancd03@estudiantes.unileon.es>
2025-11-10 13:20:51 +0100

    update

--- install.sh
+++ install.sh
@@ -50,26 +50,6 @@ echo
 sudo apt install -y gnome-control-center git
 
 echo
-echo -e "${BLUE}========================================${RESET}"
-echo -e "${BLUE}  Installing Docker${RESET}"
-echo -e "${BLUE}========================================${RESET}"
-echo
-
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-echo
 echo -e "${MAGENTA}========================================${RESET}"
 echo -e "${MAGENTA}  Installing ROS 2 Jazzy & Gazebo Harmonic${RESET}"
 echo -e "${MAGENTA}========================================${RESET}"
