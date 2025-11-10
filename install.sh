cb0412b7536686b9c51ddc4f3506f21e94e3eef0
Sergio Sanchez <ssancd03@estudiantes.unileon.es>
2025-11-10 13:07:27 +0100

    update docker

--- install.sh
+++ install.sh
@@ -54,15 +54,17 @@ echo -e "${BLUE}========================================${RESET}"
 echo -e "${BLUE}  Installing Docker${RESET}"
 echo -e "${BLUE}========================================${RESET}"
 echo
+
 sudo apt-get update
-sudo apt-get install -y ca-certificates curl
+sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \$(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo groupmod -g 1000 docker
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo
 echo -e "${MAGENTA}========================================${RESET}"
